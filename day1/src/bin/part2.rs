050f1551e65d3ace6f9eb434e07756659fa24b2f
Tamo <irevoire@protonmail.ch>
2025-12-03 14:36:35 +0100

    add day1 part2

---
+++ day1/src/bin/part2.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let n = aoc::parser::lines::<String>()
+        .flat_map(|dir| {
+            let (dir, n) = dir.split_at(1);
+            let n: usize = n.parse().unwrap();
+            std::iter::repeat_n(format!("{dir}1"), n)
+        })
+        .scan(50isize, |point, dir| {
+            let n = match dir.split_at(1) {
+                ("L", n) => -n.parse::<isize>().unwrap(),
+                ("R", n) => n.parse::<isize>().unwrap(),
+                _ => unreachable!(),
+            };
+            *point = (*point + n + 100) % 100;
+            Some(*point)
+        })
+        .filter(|n| *n == 0)
+        .count();
+    println!("{n}");
+}
