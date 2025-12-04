6e4882a7cc3a87385da0c18e4b255ef9ebd9b505
Tamo <irevoire@protonmail.ch>
2025-12-04 11:55:10 +0100

    day3 p1 broken

---
+++ day4/src/bin/part1.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let grid = aoc::parser::lines()
+        .map(|line: String| line.chars().collect())
+        .collect();
+    let grid = aoc::Grid::from(grid);
+    let ret = grid
+        .enumerate()
+        .filter(|(_, val)| **val == '@')
+        .map(|(coord, _)| {
+            (
+                coord,
+                coord
+                    .chebyshev_coords_at_distance(1)
+                    .iter()
+                    .filter_map(|coord| grid.get(*coord))
+                    .filter(|val| **val == '@')
+                    .count(),
+            )
+        })
+        .filter(|(_, count)| *count < 4)
+        .map(|(c, _)| c)
+        .collect::<Vec<_>>();
+
+    // redraw the map for fun
+    let grid = grid.map_with_coord(|c, v| if ret.contains(&c) { 'x' } else { v });
+    println!("{grid}");
+    println!("{}", ret.len());
+}
